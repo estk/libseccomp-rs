9ecfb16593f66a94304b0b7e4a25cc0be9ac11ac
Evan Simmons <esims89@gmail.com>
2023-04-10 11:15:28 -0700

    cleanup some old ideas

--- libseccomp-gen/include/wrapper.h
+++
@@ -1,1 +0,0 @@
-#include "seccomp.h.in"
--- libseccomp-sys/build.rs
+++ libseccomp-sys/build.rs
@@ -3,7 +3,6 @@
 // Copyright 2021 Sony Group Corporation
 //
 
-use libloading::{Library, Symbol};
 use std::{
     env,
     fmt::Display,
@@ -36,73 +35,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("cargo:rustc-link-lib=seccomp");
     }
     Ok(())
-}
-
-fn get_version() -> Result<(), Box<dyn std::error::Error>> {
-    let (link_type, path) = if let Ok(path) = env::var(LIBSECCOMP_LIB_PATH) {
-        let link_type = env::var(LIBSECCOMP_LINK_TYPE)
-            .map_or(Ok(LinkType::Static), |s| LinkType::from_str(&s))?;
-        (link_type, path)
-    } else if let Ok(path) = dpkg_query() {
-        (LinkType::Dylib, path)
-    } else {
-        println!("cargo:warning=WARNING: unable to find libseccomp");
-        return Ok(());
-    };
-
-    println!("cargo:rustc-link-search=native={}", path);
-
-    if matches!(link_type, LinkType::Dylib) {
-        unsafe {
-            let lib = Library::new(path)?;
-            let scver_fn: Symbol<unsafe extern "C" fn() -> *const scmp_version> =
-                lib.get(b"seccomp_version")?;
-            let res = scver_fn();
-            eprintln!(
-                "cargo:warning=SCVER: {}.{}.{}",
-                (*res).major,
-                (*res).minor,
-                (*res).micro
-            );
-        }
-    }
-    Ok(())
-}
-#[repr(C)]
-struct scmp_version {
-    major: u32,
-    minor: u32,
-    micro: u32,
-}
-fn dpkg_query() -> io::Result<String> {
-    let Output {
-        status,
-        stderr,
-        stdout,
-    } = Command::new("dpkg-query")
-        .args(["-L", "libseccomp2"])
-        .output()?;
-    if status.success() {
-        let strout = String::from_utf8(stdout)
-            .map_err(|e| io::Error::new(ErrorKind::Other, format!("Utf error: {e}")))?;
-        let lsc_so = strout.lines().find(|l| l.ends_with("libseccomp.so.2"));
-        if let Some(so) = lsc_so {
-            Ok(so.into())
-        } else {
-            Err(io::Error::new(
-                ErrorKind::Other,
-                "Unable to find libseccomp.so.2",
-            ))
-        }
-    } else {
-        Err(io::Error::new(
-            ErrorKind::Other,
-            format!(
-                "error running run dpkg-query: {}",
-                String::from_utf8_lossy(&stderr)
-            ),
-        ))
-    }
 }
 
 #[derive(Debug)]
